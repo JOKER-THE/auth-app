636444e70245f2d6d2865153ed4b5105d049eee1
Madness-0f-J0KER <pavel11_06@mail.ru>
2020-04-03 17:21:29 +0800

    form

---
+++ src/components/AuthForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="auth">
+    <form @submit.prevent="login">
+        <h1>Авторизация</h1>
+        <input required v-model="username" type="text" placeholder="Логин"/><br>
+        <input required v-model="password" type="password" placeholder="Пароль"/><br>
+        <button type="submit">Войти</button>
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AuthForm',
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      console.log(this.username, this.password)
+    }
+  }
+};
+
+</script>
