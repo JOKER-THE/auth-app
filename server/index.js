72ecc963b0756e652d1f79be6a2b38c772057fb6
Madness-0f-J0KER <pavel11_06@mail.ru>
2020-04-04 13:21:02 +0800

    get user --test

--- server/index.js
+++ server/index.js
@@ -1,17 +1,36 @@
+class User {
+	constructor(username, password) {
+		this.username = username
+		this.password = password
+	}
+}
+module.export = User
+
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 const graphqlHTTP = require('express-graphql');
 const cors = require('cors');
 
+const users = [
+  new User('Bruce', 'Wayne'),
+  new User('Alfred', 'Penniort')
+];
+
 const typeDefs = gql`
   type Query {
     hello: String
+    getUsers: [User]
+  }
+  type User {
+  	username: String
+  	password: String
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
+    getUsers: () => users
   },
 };
 
--- src/components/AuthForm.vue
+++ src/components/AuthForm.vue
@@ -6,22 +6,44 @@
         <input required v-model="password" type="password" placeholder="Пароль"/><br>
         <button type="submit">Войти</button>
     </form>
+    <div>
+      Data: 
+      <div v-for="user in users" :key="user.username">
+        {{ user }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   name: 'AuthForm',
   data() {
     return {
       username: '',
-      password: ''
+      password: '',
+      users: []
     }
   },
   methods: {
-    login() {
-      console.log(this.username, this.password)
+    async login() {
+      try {
+        const res = await axios.post(
+          'http://localhost:4000/graphql', {
+            query: `{ 
+              getUsers {
+                username
+                password
+              }
+            }`
+          }
+        )
+        this.users = res.data.data
+      } catch (e) {
+        console.log('err', e)
+      }
     }
   }
 };
