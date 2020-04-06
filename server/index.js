04bb2cc060cb4a2ae000a2659d7309728a8d2353
Madness-0f-J0KER <pavel11_06@mail.ru>
2020-04-06 14:33:20 +0800

    JWT

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 class User {
-  constructor(id, username, password, token) {
+  constructor(id, username, password) {
     this.id = id
     this.username = username
     this.password = password
-    this.token = token
+    this.token = JWT(id, username)
   }
 }
 module.export = User
+
+function JWT(id, username) {
+  var header = '{"alg": "none", "typ": "JWT"}'
+  var payload = '{"id": '+ id +', "username": ' + username + '}'
+  var headerBase64 = Buffer.from(header).toString('base64')
+  var payloadBase64 = Buffer.from(payload).toString('base64')
+
+  return headerBase64 + '.' + payloadBase64
+}
 
 const express = require('express')
 const { graphql, buildSchema } = require('graphql')
@@ -14,8 +23,8 @@ const graphqlHTTP = require('express-graphql')
 const cors = require('cors')
 
 const users = [
-  new User('0000001', 'Bruce', 'Wayne', 'this-is-a-batman'),
-  new User('0000002', 'Alfred', 'Penniort', 'this-is-a-butler')
+  new User('0000001', 'Bruce', 'Wayne'),
+  new User('0000002', 'Alfred', 'Penniort')
 ]
 
 const schema = buildSchema(`
